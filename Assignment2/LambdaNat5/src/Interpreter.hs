0a1891b755a990ec1046b953ef7238e1e32fc5ed
SEED <brumm@chapman.edu>
2021-11-06 00:55:22 -0400

    Added subst for all functions

--- Assignment2/LambdaNat5/src/Interpreter.hs
+++ Assignment2/LambdaNat5/src/Interpreter.hs
@@ -83,4 +83,8 @@ subst id s (EPlus e l) = EPlus (subst id s e) (subst id s l)
 subst id s (EMinus e l) = EMinus (subst id s e) (subst id s l)
 subst id s (ETimes e l) = ETimes (subst id s e) (subst id s l)
 -- add the missing cases
-
+subst id s ENil = ENil
+subst id s (ECons e l) = ECons (subst id s e) (subst id s l)
+subst id s (EHd e) = EHd (subst id s e)
+subst id s (ETl e) = ETl (subst id s e)
+subst id s (ELE e l) = ELE (subst id s e) (subst id s l)
