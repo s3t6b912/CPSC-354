7501355e32374b3c5823963d3d55bbbe51653ca5
SEED <brumm@chapman.edu>
2021-11-07 01:27:37 -0500

    Adding solutions.hs with some functions

---
+++ Assignment2/LambdaNat5/test/solutions.hs
@@ -0,0 +1,20 @@
+mymember :: Eq a => a -> [a] -> Bool
+mymember elem list = if elem == head(list) then True else if length(list) == 1 then False else mymember (elem) (tail(list))
+
+myremove :: Eq a => a -> [a] -> [a]
+myremove elem list = if elem == head(list) then tail(list) else if length(list) == 1 then list else head(list) : myremove (elem) (tail(list))
+
+mysum :: [Integer] -> Integer
+mysum list = if length(list) == 1 then head(list) else head(list)+(mysum (tail(list)))
+
+myprod :: [Integer] -> Integer
+myprod list = if length(list) == 1 then head(list) else head(list)*(myprod (tail(list)))
+
+main = do
+    let a = [2,3,1]
+    let b = [1,2,3,1,2,3]
+    let c = [1,2,3,4]
+    print $ mymember (1) (a)
+    print $ myremove (2) (b)
+    print $ mysum (c)
+    print $ myprod (c)
